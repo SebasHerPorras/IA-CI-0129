02e43f2274a2cd15299d53c7fe2f89caf2852894
SebasHerPorras <hernandezporrassebastian@gmail.com>
2025-09-09 19:11:57 -0600

    algoritmo de ancho primero

---
+++ Tarea3/Anchoprimero.py
@@ -0,0 +1,28 @@
+from collections import deque
+from puzzle import obtener_movimientos, mover
+
+def anchoPrimero(inicial, objetivo):
+    """
+    Búsqueda en anchura (BFS) usando deque
+    """
+    queue = deque([(inicial, [])])
+    visitados = set()
+
+    while queue:
+        estado, camino = queue.popleft()
+
+        if estado == objetivo:
+            return camino + [estado]
+
+        estado_hash = tuple(tuple(fila) for fila in estado)
+        if estado_hash in visitados:
+            continue
+        visitados.add(estado_hash)
+
+        f0, c0 = next((i, j) for i in range(3) for j in range(3) if estado[i][j] == 0)
+
+        for fn, cn in obtener_movimientos(f0, c0):
+            nuevo_estado = mover(estado, f0, c0, fn, cn)
+            queue.append((nuevo_estado, camino + [estado]))
+
+    return None
